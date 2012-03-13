8bb0d52edb5483219b607449bf2b7126d18407ab
eirik_aasved <eirik_aasved@hotmail.com>
2012-03-13 13:53:44 +0000

    oki

---
+++ src/testpakke/TestKlasse.java
@@ -0,0 +1,9 @@
+package testpakke;
+
+public class TestKlasse {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+    System.out.println("røsk meg i anus!");
+ }
+}
