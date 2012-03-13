80bd59e3f86e08c46820beee7787a8e3d628c8f5
eirik_aasved <eirik_aasved@hotmail.com>
2012-03-13 13:53:44 +0000

    oki

--- src/testpakke/TestKlasse.java
+++ src/testpakke/TestKlasse.java
@@ -4,5 +4,6 @@ public class TestKlasse {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		System.out.println("Hello world!");
-	}
+    System.out.println("røsk meg i anus!");
+ }
 }
