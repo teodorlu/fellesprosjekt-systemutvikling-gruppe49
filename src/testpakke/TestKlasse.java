530f747c0e1ae44381e719b617e18c8757c84963
Dzenan <Dzenan@Dzenan-PC.wlan.ntnu.no>
2012-03-13 14:53:49 +0100

    la til en linje

--- src/testpakke/TestKlasse.java
+++ src/testpakke/TestKlasse.java
@@ -3,5 +3,9 @@ package testpakke;
 public class TestKlasse {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		////Dzenan
+		System.out.println("1+1=2");
+		////
 	}
 }
