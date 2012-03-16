17d2f78430ed049cb3234c2951d3aef7d5365cad
Teodor <Teodor@Envy14.wlan.ntnu.no>
2012-03-16 12:42:36 +0100

    La til bibliloteket for ? koble til mysql

--- src/testpakke/DBTest.java
+++ src/testpakke/DBTest.java
@@ -6,25 +6,27 @@ import java.sql.SQLException;
 
 public class DBTest {
 	public static void main(String[] args) throws Exception, Exception {
-		Connection con  =  null;
+		Connection con = null;
 		try {
-		  Class.forName("com.mysql.jdbc.Driver").newInstance();
-		  String url = "jdbc:mysql://mysql.stud.ntnu.no/magnurod_venner";
-		  String user = "magnurod_test";
-		  String pw = "enbunt";
-		  con = DriverManager.getConnection(url,user,pw);
-		  System.out.println("Tilkoblingen fungerte.");
-		  } catch (SQLException ex) {
-		    System.out.println("Tilkobling feilet: "+ex.getMessage());
-		  } catch (ClassNotFoundException ex) {
-		    System.out.println("Feilet under driverlasting: "+ex.getMessage());
-		  } finally {
-		    try {
-		      if (con !=  null) con.close();
-		    } catch (SQLException ex) {
-		      System.out.println("Epic fail: "+ex.getMessage());
-		    }
-		  }
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			String url = "jdbc:mysql://mysql.stud.ntnu.no/magnurod_venner";
+			String user = "magnurod_test";
+			String pw = "enbunt";
+			con = DriverManager.getConnection(url, user, pw);
+			System.out.println("Tilkoblingen fungerte.");
+		} catch (SQLException ex) {
+			System.out.println("Tilkobling feilet: " + ex.getMessage());
+		} catch (ClassNotFoundException ex) {
+			System.out
+					.println("Feilet under driverlasting: " + ex.getMessage());
+			System.out.println(ex);
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException ex) {
+				System.out.println("Epic fail: " + ex.getMessage());
+			}
+		}
 	}
 }
-
