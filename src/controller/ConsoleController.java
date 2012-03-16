0427363123838c24a583f0082b595971b66ea597
Teodor <Teodor@Envy14.wlan.ntnu.no>
2012-03-16 14:09:50 +0100

    !

--- src/controller/ConsoleController.java
+++ src/controller/ConsoleController.java
@@ -1,5 +1,18 @@
 package controller;
 
-public class ConsoleController {
+import java.io.InputStream;
 
+public class ConsoleController {
+	
+	private InputStream stream;
+	
+	public ConsoleController(InputStream stream){
+		String s = System.console().readLine();
+		System.console().printf(s);
+	}
+	
+	public static void main(String[] args) {
+		new ConsoleController(System.in);
+	}
+	
 }
