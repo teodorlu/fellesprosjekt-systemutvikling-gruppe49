c1df068bb1fea54a726be42a1f9dd946581e8fbe
Magnus <Magnus@Magnus-PC.wlan.ntnu.no>
2012-03-16 12:25:29 +0100

    DBtest, connection

---
+++ src/testpakke/DBTest.java
@@ -0,0 +1,30 @@
+package testpakke;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBTest {
+	public static void main(String[] args) throws Exception, Exception {
+		Connection con  =  null;
+		try {
+		  Class.forName("com.mysql.jdbc.Driver").newInstance();
+		  String url = "jdbc:mysql://mysql.stud.ntnu.no/magnurod_venner";
+		  String user = "magnurod_test";
+		  String pw = "enbunt";
+		  con = DriverManager.getConnection(url,user,pw);
+		  System.out.println("Tilkoblingen fungerte.");
+		  } catch (SQLException ex) {
+		    System.out.println("Tilkobling feilet: "+ex.getMessage());
+		  } catch (ClassNotFoundException ex) {
+		    System.out.println("Feilet under driverlasting: "+ex.getMessage());
+		  } finally {
+		    try {
+		      if (con !=  null) con.close();
+		    } catch (SQLException ex) {
+		      System.out.println("Epic fail: "+ex.getMessage());
+		    }
+		  }
+	}
+}
+
