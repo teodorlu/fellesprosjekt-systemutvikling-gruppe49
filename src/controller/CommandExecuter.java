381dee02780b8c4c2e8f1f6ae7b8a237d601295f
Dzenan <Dzenan@Dzenan-PC.wlan.ntnu.no>
2012-03-16 16:48:12 +0100

    Test

--- src/controller/CommandExecuter.java
+++ src/controller/CommandExecuter.java
@@ -5,32 +5,40 @@ import java.util.List;
 
 public class CommandExecuter {
 	
-	public void register(String[] array){
+	public static void register(String[] array){
 		
 		List<String> liste = Arrays.asList(array);
+		String password = "Feil";
+		String username = "Feil";
+		String firstName = "Feil";
+		String lastName = "Feil";
 		
 		if(liste.contains("-p")){
 			int pIndex = liste.indexOf("-p");
-			String password = liste.get(pIndex+1);
+			password = liste.get(pIndex+1);
 		}
 		
 		if(liste.contains("-u")){
 			int uIndex = liste.indexOf("-u");
-			String username = liste.get(uIndex+1);
+			username = liste.get(uIndex+1);
 		}
 		
 		if(liste.contains("-fn")){
 			int fnIndex = liste.indexOf("-u");
-			String firstName = liste.get(fnIndex+1);
+			firstName = liste.get(fnIndex+1);
 		}
 		
 		if(liste.contains("-ln")){
 			int lnIndex = liste.indexOf("-u");
-			String lastName = liste.get(lnIndex+1);
+			lastName = liste.get(lnIndex+1);
 		}
 		
 		System.out.println(username +" "+ password+" "+ firstName+" "+" "+lastName);
 		
+	}
+	
+	public static void main(String args[]){
+		String[] lol = {"register, -u, dzenan, -p, mittpassord, -fn, -ln"};
 	}
 
 }
