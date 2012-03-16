078d9978f0253cad7721579c6dd388a27fa4915f
Teodor <Teodor@Envy14.wlan.ntnu.no>
2012-03-16 17:31:56 +0100

    Appointment implements compareTo on startTime

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -8,45 +8,37 @@ import java.util.Date;
  * Metoder for å beregne endTime ut ifra startTime og appLength
  * +++
  */
-public class Appointment implements Comparable<Appointment>{
-	
-	
-	private Date startTime;
-	private Date endTime;
-	private Time appLength;
-	
-	private String description;
-	private String place;
-	private String title;
-	
+public class Appointment implements Comparable<Appointment> {
 
-	
-	public Appointment(Date startTime, Time appLength,String title, String description, String place){
-	
-	this.startTime = startTime;
-	this.appLength = appLength;
-	this. description = description;
-	this.place = place;
-	this.title = title;
-	
-	
+	private Date startTime, endTime;
+	private Time appLength;
+
+	private String description, place, title;
+
+	public Appointment(Date startTime, Time appLength, String title,
+			String description, String place) {
+
+		this.startTime = startTime;
+		this.appLength = appLength;
+		this.description = description;
+		this.place = place;
+		this.title = title;
+
 	}
-	
+
 	/*
-	 * Metoder
-	public void editStartTime(){
-			
-	}
-	
-	public void editDate(){
-		
-	}
-	*/
-	
+	 * Metoder public void editStartTime(){
+	 * 
+	 * }
+	 * 
+	 * public void editDate(){
+	 * 
+	 * }
+	 */
+
 	@Override
 	public int compareTo(Appointment other) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.startTime.compareTo(other.startTime);
 	}
 
 }
