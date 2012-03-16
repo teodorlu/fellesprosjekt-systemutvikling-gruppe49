ce0ed8067971fdaa476d95eed6c0af12c9f58006
Dzenan <Dzenan@Dzenan-PC.wlan.ntnu.no>
2012-03-16 16:38:47 +0100

    Lagt til s? ? si alle variabler i Room/Appointment/Notification men ikke gjort stort mere enn dette.

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,11 +1,47 @@
 package model;
 
+import java.sql.Time;
 import java.util.Date;
 
+/* TO-DO 
+ * Må legge til en kalenderpeker! Også i konstruktøren
+ * Metoder for å beregne endTime ut ifra startTime og appLength
+ * +++
+ */
 public class Appointment implements Comparable<Appointment>{
 	
-	private Date startTime;
 	
+	private Date startTime;
+	private Date endTime;
+	private Time appLength;
+	
+	private String description;
+	private String place;
+	private String title;
+	
+
+	
+	public Appointment(Date startTime, Time appLength,String title, String description, String place){
+	
+	this.startTime = startTime;
+	this.appLength = appLength;
+	this. description = description;
+	this.place = place;
+	this.title = title;
+	
+	
+	}
+	
+	/*
+	 * Metoder
+	public void editStartTime(){
+			
+	}
+	
+	public void editDate(){
+		
+	}
+	*/
 	
 	@Override
 	public int compareTo(Appointment other) {
