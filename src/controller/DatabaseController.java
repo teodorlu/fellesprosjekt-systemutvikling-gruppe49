3088fae9e365408e96bac3a559e359ede77dfe26
Magnus <Magnus@Magnus-PC.wlan.ntnu.no>
2012-03-20 13:45:12 +0100

    sjekke username og pw

--- src/controller/DatabaseController.java
+++ src/controller/DatabaseController.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -21,8 +22,8 @@ public class DatabaseController {
 			String pw = "stabak";
 			con = DriverManager.getConnection(url, user, pw);
 			
-			if (con != null)
-				System.out.println("nice");
+//			if (con != null)
+//				System.out.println("nice");
 
 		} catch (SQLException ex) {
 			System.out.println("Tilkobling feilet: " + ex.getMessage());
@@ -53,9 +54,7 @@ public class DatabaseController {
 			Statement st = con.createStatement();
 			int res = -1;
 			res = st.executeUpdate("INSERT INTO ANSATT VALUES ( "
-					+ username + ", " + pw + ", " + fname + ", "+ lname + ", NULL, 0)" );
-			if (res != -1)
-				System.out.println("wee");
+					+ username + ", " + pw + ", " + fname + ", "+ lname + ", NULL, 0)" );;
 		} catch (InstantiationException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
@@ -66,9 +65,35 @@ public class DatabaseController {
 		disconnect();
 	}
 	
-	public boolean authenticated(User user){
-		// TODO check username and password in database
-		return true;
+	public static boolean authenticated(User user){
+		String username = user.getUsername();
+		String pw = user.getPassword();
+		String sql = "SELECT BrukerNavn, Passord FROM ANSATT WHERE BrukerNavn='"+username+"'";
+		try {
+			connect();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			String dbuser = "";
+			String dbpw = "";
+			while(rs.next()){
+				dbuser=rs.getString(1);
+				dbpw=rs.getString(2);
+			}
+			dbuser = dbuser.toLowerCase();
+			if (dbuser.equals(username) && dbpw.equals(pw)){
+				return true;
+			}
+			
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return false;
 	}
 
 	public Person retrieve(User user) {
@@ -77,11 +102,13 @@ public class DatabaseController {
 		return null;
 	}
 	
-//	public static void main(String[] args) {
-//		Person person = new Person("testEn", "pappa", "mag", "test");
-//		Save(person);
-//				
-//	}
+	public static void main(String[] args) {
+		Person person = new Person("MAGRODAHL", "mamma", "Magnus", "Rodahl");
+		if (authenticated(person)){
+			System.out.println("logget inn");
+		}
+				
+	}
 }
 
 
