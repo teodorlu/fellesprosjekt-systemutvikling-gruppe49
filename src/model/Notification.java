92bb760a27888013cbaf998fb3a919001994460d
Acekob <Acekob@Acekob-HP>
2012-03-20 14:25:22 +0100

    S?nn

--- src/model/Notification.java
+++ src/model/Notification.java
@@ -12,13 +12,15 @@ import java.util.ArrayList;
 
 public class Notification {
 	
-	private enum type {Nytt, Oppdatert, Slettet};
-	private enum reply {Ja, Nei, Ubesvart};
+	private enum eType {Nytt, Oppdatert, Slettet};
+	private enum eReply {Ja, Nei, Ubesvart};
+	eReply reply;
+	eType type;
 	private Appointment sender;
 	private ArrayList<Person> receivers;	
 	private String text;
 	
-	public Notification(String text, Appointment sender, ArrayList<Person> receivers) {
+	public Notification(String text, eReply reply, eType type, Appointment sender, ArrayList<Person> receivers) {
 		this.text = text;
 		this.type = type;
 		this.reply = reply;
