fba3d1b5f07cf2b1f52835a2e859c450834b0932
Dzenan <Dzenan@Dzenan-PC.wlan.ntnu.no>
2012-03-22 13:55:28 +0100

    Forandra appointment i CE fra -e til -d for duration

---
+++ src/controller/ConsoleController.java
@@ -0,0 +1,93 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import model.Person;
+import model.User;
+
+import application.Application;
+import application.ApplicationComponent;
+
+public class ConsoleController extends ApplicationComponent {
+	
+	private final InputStream input;
+	private final BufferedReader reader;
+	private final Map<String, Command> commands;
+	private final CommandExecuter executer;
+	
+	public ConsoleController(Application app, InputStream input){
+		super(app);
+		
+		this.input = input;
+		InputStreamReader isr = new InputStreamReader(input);
+		reader = new BufferedReader(isr);
+		commands = new HashMap<String, Command>();
+		executer = new CommandExecuter(this.getApplication()); 
+		registerCommands();
+	}
+	
+	public boolean parseNext(){
+		boolean executedCommand = true;
+		
+		try {
+			String[] command = reader.readLine().split(" ");
+			String keyword = command[0];
+			
+			if (commands.containsKey(keyword))
+				commands.get(keyword).execute(command);
+			else
+				executedCommand = false;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return executedCommand;
+	}
+	
+	private void registerCommands() {
+		
+		commands.put("register", new Command() {
+			void execute(String[] arguments) {
+				executer.register(arguments);
+			}
+		});
+		
+		commands.put("users", new Command() {
+			void execute(String[] arguments){
+				// TODO write executer.users();
+			}
+		});
+		
+		commands.put("login", new Command() {
+			void execute(String[] arguments) {
+				executer.login(arguments);
+			}
+		});
+		
+		commands.put("user", new Command() {
+			void execute(String[] arguments) {
+				executer.user(arguments);
+			}
+		});
+		
+		commands.put("edit", new Command() {
+			void execute(String[] arguments) {
+				executer.edit(arguments);
+			}
+		});
+	}
+
+	public java.util.Set<String> getCommands() {
+		return commands.keySet();
+	}
+
+}
+
+abstract class Command {
+	abstract void execute(String[] parameters);
+}
