c31fd9354becab795e04bde3fbd4f13d7f070b99
Dzenan <Dzenan@Dzenan-PC.wlan.ntnu.no>
2012-03-22 13:17:16 +0100

    edit burde funke tror jeg

---
+++ src/controller/CommandExecuter.java
@@ -0,0 +1,319 @@
+package controller;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import model.Person;
+import model.User;
+import model.Time;
+import model.Appointment;
+
+import application.Application;
+import application.ApplicationComponent;
+
+
+public class CommandExecuter extends ApplicationComponent {
+	
+	public CommandExecuter(Application app) {
+		super(app);
+	}
+
+	public void register(String[] array){
+		
+		List<String> input = Arrays.asList(array);
+		String username, password, firstName, lastName, _email;
+		
+		if(input.contains("-u")){
+			int uIndex = input.indexOf("-u");
+			
+			username = getProperty(array, uIndex+1);
+			
+			if(input.contains("-p")){
+				int pIndex = input.indexOf("-p");
+				password = getProperty(array, pIndex+1);
+
+				if(input.contains("-fn")){
+					int fnIndex = input.indexOf("-fn");
+					firstName = getProperty(array, fnIndex+1);
+				
+					if(input.contains("-ln")){
+						int lnIndex = input.indexOf("-ln");
+						lastName = getProperty(array, lnIndex+1);
+						if(input.contains("-email")){
+							int emailIndex = input.indexOf("-email");
+							_email = getProperty(array, emailIndex +1);
+							Person u = new Person(username, password, firstName, lastName, _email);
+														
+							System.out.println(_email);
+							
+							this.getApplication().getDatabaseController().Save(u);
+						}
+						else System.out.println("Feil input: Email");
+						
+						
+					}
+					else System.out.println("Feil input: Etternavn"); //Feilmelding istedenfor
+				}
+				else System.out.println("Feil input: Fornavn");
+			}
+			else System.out.println("Feil input: Password");
+		}
+		else System.out.println("Feil input: Username");
+	}
+	
+	public void login(String[] array){
+		List<String> input = Arrays.asList(array);
+		int uIndex, pIndex;
+		String username, password;
+		
+		if(input.contains("-u") && input.contains("-p")){
+			uIndex = input.indexOf("-u");
+			pIndex  =input.indexOf("-p");
+			username = getProperty(array, uIndex+1);
+			password = getProperty(array, pIndex+1);
+			System.out.println("User: "+username + " Password: "+password);
+			
+			User user = new User(username, password);
+			this.getApplication().tryLogIn(user);
+		}
+	}
+	
+	public void appointment(String[] array) throws ParseException{
+		List<String> input = Arrays.asList(array);
+		String title = "", desc = "", place = "", sStart, sEnd;
+		Date date;
+		Time startTime = new Time(0,0);
+		Time appLength = new Time(0,0);
+		int titleIndex, dateIndex, startIndex, endIndex, descIndex, placeIndex, colonIndex, length;
+				
+		if(input.contains("-title") && input.contains("-date") && input.contains("-s")
+				&& input.contains("-e")){
+			
+			titleIndex = input.indexOf("-title");
+			title = getProperty(array, titleIndex+1);
+			
+			dateIndex = input.indexOf("-date");
+			date = stringToDate(array, dateIndex+1); 
+			
+			startIndex = input.indexOf("-s");
+			sStart = getProperty(array, startIndex+1); 
+			
+			endIndex = input.indexOf("-e");
+			sEnd = getProperty(array, endIndex+1); 
+			
+			startTime = getTimeProperty(sStart);
+			appLength = getTimeProperty(sEnd);
+
+					
+			if(input.contains("-desc")){
+				descIndex = input.indexOf("-desc");
+				desc = getProperty(array, descIndex + 1);
+			}
+			
+			if(input.contains("-place")){
+				placeIndex = input.indexOf("-place");
+				place = getProperty(array, placeIndex + 1);
+			}
+			
+			//ToDo må lage en appointment gjennom konstruktøren! (Husk dato og tid lenger oppe)
+			
+			
+			System.out.println("Tittel: "+title+" Date: "+ date.toString()+" Start: "+ startTime.toString()+" End: "+ appLength.toString()+" Desc: "+ desc+" Place: "+ place);
+			Appointment a = new Appointment(date, startTime, appLength, title, desc, place);
+			this.getApplication().getCurrentlyLoggedInUser().getPersonalCalendar().addAppointment(a);
+			//------------------------------------
+		}
+		else System.out.println("Har ikke med alle parameterne til appointment");
+	}
+	
+	
+	private String getProperty(String[] array, int index){
+		
+		
+		if(index >= array.length){
+			
+			throw new IllegalArgumentException();
+		}
+		String word = array[index];
+		if(word.charAt(0)=='-'){
+			throw new IllegalArgumentException();
+		}
+		return word;		
+	}
+	
+	private static Time getTimeProperty(String s){
+		int length;
+		Time time = null;
+		if(s.contains(":")){
+			length = s.length();
+			if(length == 5){
+				time = new Time((Integer.parseInt(s.substring(0, 2))), Integer.parseInt(s.substring(3, 5)));
+			}
+			
+			if(length == 4){
+				time = new Time((Integer.parseInt(s.substring(0, 1))), Integer.parseInt(s.substring(2, 4)));
+			}
+			
+		}
+		else throw new IllegalArgumentException();
+		
+		return time;
+	}
+	
+	private static Date stringToDate(String[] array, int index) throws ParseException{
+		String _date = array[index];
+		DateFormat df = new SimpleDateFormat("YYYY-MM-DD");
+		Date date = df.parse(_date);
+		return date;	
+	}
+	
+
+	
+
+
+	
+	public void delete(String[] array){
+		String IDstring = "";
+		int ID;
+		List<String> input = Arrays.asList(array);
+		int deleteIndex = input.indexOf("delete");
+		IDstring = getProperty(array, deleteIndex+1);
+		ID = Integer.parseInt(IDstring);
+		
+		if (this.getApplication().getDatabaseController().tryDeleteAppointment(ID))
+				System.out.println("The appointment has been deleted");
+			else{
+				System.out.println("This is not a valid appointment ID");
+				System.out.println("Mulige avtaler"); //hvordan finner jeg mulige avtaler?
+			}
+	}
+	
+	public void user(String[] array){
+		List<String> input = Arrays.asList(array);
+		List<String> usernames;
+		String username;
+		int uIndex;
+		
+		if(input.contains("user") && input.size() == 2){
+			
+			uIndex = input.indexOf("user");
+			username = getProperty(array ,uIndex+1);
+			Person a;
+			a = this.getApplication().getDatabaseController().retriveUser(username);
+			System.out.println("Brukernavn: "+a.getUsername());
+			System.out.println("Fornavn: "+a.getFirstName());
+			System.out.println("Etternavn: "+a.getLastName());
+			System.out.println("E-mail: "+a.getEmail());
+		}
+		
+		else{
+			//Skriv ut en liste med alle brukernavna
+			usernames = this.getApplication().getDatabaseController().retriveUsernames();
+			for(int i = 0; i < usernames.size(); i++){
+				String name = usernames.get(i);
+				System.out.println(name);
+			}
+		}
+	}
+	
+	public void edit(String[] array){
+		List<String> input = Arrays.asList(array);
+		List<Appointment> appointments;
+		Appointment localAppointment;
+		int ID,editIndex;
+		appointments = this.getApplication().getDatabaseController().retrieveAppointments(this.getApplication().getCurrentlyLoggedInUser());
+		
+		if(input.contains("edit") && input.size() == 1){
+					
+			for(int i = 0; i < appointments.size(); i++){
+				Appointment a = appointments.get(i);
+				System.out.println("ID: "+a.getID()+" Tittel: "+a.getTitle());   
+			}
+		}
+		
+		else if(input.contains("edit") && input.size() > 1){
+			editIndex = input.indexOf("edit");
+			ID = Integer.parseInt(getProperty(array, editIndex+1));
+			for(int i = 0; i < appointments.size(); i++){
+				localAppointment = appointments.get(i);
+				
+				if(localAppointment.getID()==ID){
+					
+					if(input.contains("-title")){
+						int titleIndex = input.indexOf("-title");
+						if(this.getApplication().getDatabaseController().editAppointment(ID, "Tittel", getProperty(array, titleIndex+1))) 
+							System.out.println("Tittel ble endret til "+getProperty(array,titleIndex+1));
+					}
+						
+										
+					try {
+					if(input.contains("-date")){
+						int dateIndex = input.indexOf("-date");
+						localAppointment.setDate(stringToDate(array, dateIndex+1));
+						if(this.getApplication().getDatabaseController().editAppointment(ID, "Dato", getProperty(array, dateIndex+1))) 
+							System.out.println("Dato ble endre til: "+stringToDate(array,dateIndex+1));
+					}
+					
+					if(input.contains("-s")){
+						int startIndex = input.indexOf("-s");
+						localAppointment.setStartTime(getTimeProperty(input.get(startIndex+1)));
+						if(this.getApplication().getDatabaseController().editAppointment(ID, "Starttid", getProperty(array, startIndex+1)))
+							System.out.println("StartTid ble endre til: "+getTimeProperty(input.get(startIndex+1)));
+					}
+					
+					if(input.contains("-d")){
+						int durationIndex = input.indexOf("-d");
+						localAppointment.setAppLength(getTimeProperty(input.get(durationIndex+1)));
+						if(this.getApplication().getDatabaseController().editAppointment(ID, "Varighet", getProperty(array, durationIndex+1)))
+							System.out.println("Varighet ble endre til: "+getTimeProperty(input.get(durationIndex+1)));
+					}
+					} catch (ParseException e) {
+						e.getMessage();
+						
+					} catch (IllegalArgumentException e) {
+						e.getMessage();
+					}
+					
+					if(input.contains("-desc")){
+						int descIndex = input.indexOf("-desc");
+						if(this.getApplication().getDatabaseController().editAppointment(ID, "Beskrivelse", getProperty(array, descIndex+1)))
+							System.out.println("Beskrivelse ble endret til: "+getProperty(array, descIndex+1));
+					}
+					
+					if(input.contains("-place")){
+						int placeIndex = input.indexOf("-place");
+						if(this.getApplication().getDatabaseController().editAppointment(ID, "Sted", getProperty(array, placeIndex+1)))
+							System.out.println("Sted ble endret til: "+getProperty(array,placeIndex+1));
+					}
+								
+					break;
+				}
+				
+				
+				
+			}
+			
+		}
+		
+		
+		
+	}
+	
+	
+	public static void main(String args[]) throws ParseException{
+		String[] registertest = {"register", "-u", "dzenan", "-p", "mittpassord", "-fn", "firstName", "-ln", "lastName"};
+		String[] logintest = {"login", "-u", "Brukernavnet", "-p", "passordet"};
+		String[] appointmentTest = {"-title", "HumbugAvtale", "-date", "2012-12-04", "-s", "14:30", "-e", "15:30", 
+										"-desc", "Heisann tullemøte", "-place", "Fjellet"};
+		
+		CommandExecuter ce = new CommandExecuter(null);
+		ce.register(registertest);
+		ce.login(logintest);
+		ce.appointment(appointmentTest);
+	}
+
+}
---
+++ src/controller/DatabaseController.java
@@ -0,0 +1,265 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import application.Application;
+import application.ApplicationComponent;
+
+import model.Appointment;
+import model.Person;
+import model.User;
+import model.Time;
+
+public class DatabaseController extends ApplicationComponent {
+	
+	private Connection con;
+	
+	public DatabaseController(Application app){
+		super(app);
+	}
+	
+	private void connect() {
+		con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			String url = "jdbc:mysql://mysql.stud.ntnu.no/magnurod_avtalekalender";
+			String user = "magnurod_fellesp";
+			String pw = "stabak";
+			con = DriverManager.getConnection(url, user, pw);
+			
+//			if (con != null)
+//				System.out.println("nice");
+
+		} catch (SQLException ex) {
+			System.out.println("Tilkobling feilet: " + ex.getMessage());
+		} catch (ClassNotFoundException ex) {
+			System.out
+					.println("Feilet under driverlasting: " + ex.getMessage());
+			System.out.println(ex);
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void disconnect() {
+		try {
+			if (con != null)
+				con.close();
+		} catch (SQLException ex) {
+			System.out.println("Epic fail: " + ex.getMessage());
+		}
+	}
+	
+	public void Save(Person user) {
+		String username = incapsulate(user.getUsername());
+		String pw = incapsulate(user.getPassword());
+		String fname = incapsulate(user.getFirstName());
+		String lname = incapsulate(user.getLastName());
+		String email = incapsulate(user.getEmail());
+		String sql = "INSERT INTO ANSATT VALUES ( "
+					+ username + ", " + pw + ", " + fname + ", "+ lname + ", "+ email +", 0)";
+		try {
+			connect();
+			Statement st = con.createStatement();
+			int res = st.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect();
+	}
+	
+	public boolean authenticated(User user){
+		String username = user.getUsername();
+		String pw = user.getPassword();
+		String sql = "SELECT BrukerNavn, Passord FROM ANSATT WHERE BrukerNavn='"+username+"'";
+		try {
+			connect();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			String dbuser = "";
+			String dbpw = "";
+			while(rs.next()){
+				dbuser=rs.getString(1);
+				dbpw=rs.getString(2);
+			}
+			dbuser = dbuser.toLowerCase();
+			if (dbuser.equals(username) && dbpw.equals(pw)){
+				return true;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+		return false;
+	}
+
+	public List<Appointment> retrieveAppointments(User user) {
+
+		String username = user.getUsername();
+		List<Appointment> listOfApp = new ArrayList<Appointment>();
+		String sql = "SELECT * FROM AVTALE WHERE AvtaleEier='"+username+"' " +
+				"AND ErAktiv=1 AND TYPE=Avtale";
+		try {
+			connect();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+			while(rs.next()){
+				Appointment a = new Appointment( rs.getInt(1), rs.getDate(3), rs.getTime(8), 
+						rs.getTime(9), rs.getString(2), rs.getString(10), rs.getString(6));
+				listOfApp.add(a);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect();
+		return listOfApp;
+	}
+	
+	public void updateLoginStatus(User user, boolean isOnline){
+		String username = user.getUsername();
+		String sql = "UPDATE ANSATT SET IsLoggedOn="+isOnline+" WHERE BrukerNavn='"+username+"'";
+		try {
+			connect();
+			Statement st = con.createStatement();
+			st.executeUpdate(sql);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect();
+	}
+	
+	public boolean getOnlineStatus(String username){
+		boolean isOnline;
+		String sql = "SELECT IsLoggedOn FROM ANSATT WHERE BrukerNavn='"+username+"'";
+		try {
+			connect();
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next()){
+				isOnline = rs.getBoolean(1);
+				disconnect();
+				return isOnline;
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		disconnect();
+		return false;
+	}
+	
+	public Person retriveUser(String username){
+		// TODO write
+		String sql = "SELECT * FROM ANSATT WHERE BrukerNavn='"+username+"'";
+		connect();
+		Statement st;
+		try {
+			st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			Person user;
+			while(rs.next()){
+				user = new Person(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5));
+				disconnect();
+				return user;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		disconnect();
+		return null;
+		
+	}
+	
+	public List<String> retriveUsernames(){
+		String sql = "SELECT BrukerNavn FROM ANSATT";
+		List<String> listOfUsernames = new ArrayList<String>();
+		connect();
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next()){
+				listOfUsernames.add(rs.getString(1));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		disconnect();
+		return listOfUsernames;
+	}
+	
+	public int[] deleteUser(String username){
+		String sql = "DELETE FROM ANSATT WHERE BrukerNavn='"+username+"'";
+		String sql2 = "DELETE FROM PAMINNELSE WHERE Paminner='"+username+"'";
+		int rowseffected[] = {-1,-1};
+		connect();
+		try {
+			Statement st = con.createStatement();
+			st.addBatch(sql);
+			st.addBatch(sql2);
+			rowseffected = st.executeBatch();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		disconnect();
+		return rowseffected;
+	}
+	
+	public boolean tryDeleteAppointment(int ID){
+		String sql = "UPDATE AVTALE SET ErAktiv=0 WHERE AvtaleID="+ID;
+		int res=-1;
+		connect();
+		try {
+			Statement st = con.createStatement();
+			res = st.executeUpdate(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		disconnect();
+		if (res >= 1)
+			return true;
+		return false;
+	}
+	
+	
+	
+	private String incapsulate(String input){
+		return "'" + input + "'";
+	}
+	
+	private String sqlInsert(String tabellnavn, Collection<String> properties){
+		// TODO: write
+		return null;
+	}
+	
+//	public static void main(String[] args) {
+//		DatabaseController dbc = new DatabaseController(null);
+//		dbc.retriveUsernames();
+//
+//		
+//	
+//	}
+}
+
+
+
