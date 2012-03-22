49c73426d9c3efb13297d33c2783349c45052098
Dzenan <Dzenan@Dzenan-PC.wlan.ntnu.no>
2012-03-22 12:09:38 +0100

    La til user commandoen til applicationen, og fiksa en liten endring av en kommando i Application. User funker slik jeg ser d

--- src/application/Application.java
+++ src/application/Application.java
@@ -81,7 +81,7 @@ public class Application {
 		boolean isValid = getDatabaseController().authenticated(user);
 		
 		if (isValid) {
-			Person currentlyLoggedInPerson = getDatabaseController().retrieve(user);
+			Person currentlyLoggedInPerson = getDatabaseController().retriveUser(user.getUsername());    //Dzenan enda denne, se på denne om alt går mongoklikk!!!
 			this.setCurrentUser(currentlyLoggedInPerson);
 			this.getConsoleView().showSucessfulLoginMessage(currentlyLoggedInPerson);
 		}
