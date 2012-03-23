5a4fc80f3ce1300357e8ac5a49eb716875dcec8d
Teodor <Teodor@Envy14.wlan.ntnu.no>
2012-03-23 12:03:10 +0100

    La til tester med struktur

--- src/controller/CommandParser.java
+++ src/controller/CommandParser.java
@@ -11,20 +11,30 @@ public class CommandParser {
 	private Map<String, List<String>> mustContainOneOf;
 	private Map<String, List<String>> mustContainAllOf;
 	private List<String> implicit;
+	private List<String> multipleImplicit;
 	
 	public CommandParser(String[] format){
 		canContainOneOf = new HashMap<String, List<String>>();
 		mustContainOneOf = new HashMap<String, List<String>>();
 		mustContainAllOf = new HashMap<String, List<String>>();
 		implicit = new LinkedList<String>();
-		build();
+		multipleImplicit = new LinkedList<String>();
+		build(format);
 	}
 	
-	private void build() {
+	private void build(String[] format) {
 		
+		
+	}
+	
+	public static String removeBraces(String source) {
+		if (!(source.charAt(0) == '<' && source.charAt(source.length() - 1) == '>'))
+			throw new IllegalArgumentException("source did not contain braces");
+		return source.substring(1, source.length() - 1);
 		
 	}
 
+	@SuppressWarnings("unused") // Preventing construction without format
 	private CommandParser(){
 		
 	}
