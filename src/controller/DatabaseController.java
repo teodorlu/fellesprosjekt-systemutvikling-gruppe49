9d7f6c0be4b959606257781dea66afae4ee12a67
Magnus <Magnus@Magnus-PC.wlan.ntnu.no>
2012-03-27 15:28:02 +0200

    retrieveNotifications og enums

--- src/controller/DatabaseController.java
+++ src/controller/DatabaseController.java
@@ -7,26 +7,46 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.print.attribute.HashDocAttributeSet;
 
 import application.Application;
 import application.ApplicationComponent;
 
-import model.Appointment;
-import model.Meeting;
-import model.Person;
-import model.Room;
-import model.User;
-import model.Time;
+import model.*;
 
 public class DatabaseController extends ApplicationComponent {
-	
+
 	private Connection con;
-	
-	public DatabaseController(Application app){
+	private Map<String,NotificationType> string2enum;
+	private Map<Integer,ReplyStatus> int2enum;
+
+	public DatabaseController(Application app) {
 		super(app);
+		buildString2enum();
+		buildInt2enum();
 	}
 	
+
+	private void buildInt2enum() {
+		int2enum = new HashMap<Integer, ReplyStatus>();
+		int2enum.put(-1, ReplyStatus.UBESVART);
+		int2enum.put(0, ReplyStatus.NEI);
+		int2enum.put(1,ReplyStatus.JA);
+	}
+
+
+	private void buildString2enum() {
+		string2enum = new HashMap<String,NotificationType>();
+		string2enum.put("Nytt", NotificationType.NYTT);
+		string2enum.put("Oppdatert", NotificationType.OPPDATERT);
+		string2enum.put("Slettet", NotificationType.SLETTET);
+	}
+
+
 	private void connect() {
 		con = null;
 		try {
@@ -49,7 +69,7 @@ public class DatabaseController extends ApplicationComponent {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void disconnect() {
 		try {
 			if (con != null)
@@ -58,15 +78,15 @@ public class DatabaseController extends ApplicationComponent {
 			System.out.println("Epic fail: " + ex.getMessage());
 		}
 	}
-	
+
 	public void Save(Person user) {
 		String username = incapsulate(user.getUsername());
 		String pw = incapsulate(user.getPassword());
 		String fname = incapsulate(user.getFirstName());
 		String lname = incapsulate(user.getLastName());
 		String email = incapsulate(user.getEmail());
-		String sql = "INSERT INTO ANSATT VALUES ( "
-					+ username + ", " + pw + ", " + fname + ", "+ lname + ", "+ email +", 0)";
+		String sql = "INSERT INTO ANSATT VALUES ( " + username + ", " + pw
+				+ ", " + fname + ", " + lname + ", " + email + ", 0)";
 		try {
 			connect();
 			Statement st = con.createStatement();
@@ -76,22 +96,27 @@ public class DatabaseController extends ApplicationComponent {
 		}
 		disconnect();
 	}
-	
-	public boolean newAppointment(Appointment appointment){
+
+	public boolean newAppointment(Appointment appointment) {
 		String title = incapsulate(appointment.getTitle());
 		java.util.Date date = appointment.getDate();
-		java.sql.Date sqlDate = new java.sql.Date(date.getYear(),date.getMonth(),date.getDate());
-		String owner = incapsulate(this.getApplication().getCurrentlyLoggedInUser().getUsername());
+		java.sql.Date sqlDate = new java.sql.Date(date.getYear(),
+				date.getMonth(), date.getDate());
+		String owner = incapsulate(this.getApplication()
+				.getCurrentlyLoggedInUser().getUsername());
 		String place = incapsulate(appointment.getPlace());
 		Time startTime = appointment.getStartTime();
-		java.sql.Time sqlStartTime = new java.sql.Time(startTime.returnHours(),startTime.returnMinutes(),00);
+		java.sql.Time sqlStartTime = new java.sql.Time(startTime.returnHours(),
+				startTime.returnMinutes(), 00);
 		Time duration = appointment.getAppLength();
-		java.sql.Time sqlDuration = new java.sql.Time(duration.returnHours(),duration.returnMinutes(),00);
+		java.sql.Time sqlDuration = new java.sql.Time(duration.returnHours(),
+				duration.returnMinutes(), 00);
 		String desc = incapsulate(appointment.getDescription());
-		String sql = "INSERT INTO AVTALE (Tittel, Dato, AvtaleEier, TYPE, " +
-				"Sted, Starttid, Varighet, Beskrivelse, ErAktiv )" +
-				"VALUES ( " + title + ", '"+sqlDate+"', "+owner+", " +
-				"'Avtale', "+ place + ", '"+sqlStartTime+"', '"+sqlDuration+"', " + desc +", 1)";
+		String sql = "INSERT INTO AVTALE (Tittel, Dato, AvtaleEier, TYPE, "
+				+ "Sted, Starttid, Varighet, Beskrivelse, ErAktiv )"
+				+ "VALUES ( " + title + ", '" + sqlDate + "', " + owner + ", "
+				+ "'Avtale', " + place + ", '" + sqlStartTime + "', '"
+				+ sqlDuration + "', " + desc + ", 1)";
 		connect();
 		int rowsAffected = -1;
 		try {
@@ -106,26 +131,27 @@ public class DatabaseController extends ApplicationComponent {
 			return true;
 		return false;
 	}
-	
-	public boolean authenticated(User user){
+
+	public boolean authenticated(User user) {
 		String username = user.getUsername();
 		String pw = user.getPassword();
-		String sql = "SELECT BrukerNavn, Passord FROM ANSATT WHERE BrukerNavn='"+username+"'";
+		String sql = "SELECT BrukerNavn, Passord FROM ANSATT WHERE BrukerNavn='"
+				+ username + "'";
 		try {
 			connect();
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
 			String dbuser = "";
 			String dbpw = "";
-			while(rs.next()){
-				dbuser=rs.getString(1);
-				dbpw=rs.getString(2);
+			while (rs.next()) {
+				dbuser = rs.getString(1);
+				dbpw = rs.getString(2);
 			}
 			dbuser = dbuser.toLowerCase();
-			if (dbuser.equals(username) && dbpw.equals(pw)){
+			if (dbuser.equals(username) && dbpw.equals(pw)) {
 				return true;
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -138,16 +164,17 @@ public class DatabaseController extends ApplicationComponent {
 
 		String username = user.getUsername();
 		List<Appointment> appointments = new ArrayList<Appointment>();
-		String sql = "SELECT * FROM AVTALE WHERE AvtaleEier='"+username+"' " +
-				"AND ErAktiv=1 AND TYPE='Avtale'";
+		String sql = "SELECT * FROM AVTALE WHERE AvtaleEier='" + username
+				+ "' " + "AND ErAktiv=1 AND TYPE='Avtale'";
 		try {
 			connect();
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			
-			while(rs.next()){
-				Appointment a = new Appointment( rs.getInt(1), rs.getDate(3), rs.getTime(8), 
-						rs.getTime(9), rs.getString(2), rs.getString(10), rs.getString(7));
+
+			while (rs.next()) {
+				Appointment a = new Appointment(rs.getInt(1), rs.getDate(3),
+						rs.getTime(8), rs.getTime(9), rs.getString(2),
+						rs.getString(10), rs.getString(7));
 				appointments.add(a);
 			}
 		} catch (SQLException e) {
@@ -156,29 +183,31 @@ public class DatabaseController extends ApplicationComponent {
 		disconnect();
 		return appointments;
 	}
-	
-	public void updateLoginStatus(User user, boolean isOnline){
+
+	public void updateLoginStatus(User user, boolean isOnline) {
 		String username = user.getUsername();
-		String sql = "UPDATE ANSATT SET IsLoggedOn="+isOnline+" WHERE BrukerNavn='"+username+"'";
+		String sql = "UPDATE ANSATT SET IsLoggedOn=" + isOnline
+				+ " WHERE BrukerNavn='" + username + "'";
 		try {
 			connect();
 			Statement st = con.createStatement();
 			st.executeUpdate(sql);
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		disconnect();
 	}
-	
-	public boolean getOnlineStatus(String username){
+
+	public boolean getOnlineStatus(String username) {
 		boolean isOnline;
-		String sql = "SELECT IsLoggedOn FROM ANSATT WHERE BrukerNavn='"+username+"'";
+		String sql = "SELECT IsLoggedOn FROM ANSATT WHERE BrukerNavn='"
+				+ username + "'";
 		try {
 			connect();
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			while(rs.next()){
+			while (rs.next()) {
 				isOnline = rs.getBoolean(1);
 				disconnect();
 				return isOnline;
@@ -191,18 +220,19 @@ public class DatabaseController extends ApplicationComponent {
 		disconnect();
 		return false;
 	}
-	
-	public Person retriveUser(String username){
+
+	public Person retriveUser(String username) {
 		// TODO write
-		String sql = "SELECT * FROM ANSATT WHERE BrukerNavn='"+username+"'";
+		String sql = "SELECT * FROM ANSATT WHERE BrukerNavn='" + username + "'";
 		connect();
 		Statement st;
 		try {
 			st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
 			Person user;
-			while(rs.next()){
-				user = new Person(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5));
+			while (rs.next()) {
+				user = new Person(rs.getString(1), rs.getString(2),
+						rs.getString(3), rs.getString(4), rs.getString(5));
 				disconnect();
 				return user;
 			}
@@ -212,17 +242,17 @@ public class DatabaseController extends ApplicationComponent {
 		}
 		disconnect();
 		return null;
-		
+
 	}
-	
-	public List<String> retriveUsernames(){
+
+	public List<String> retriveUsernames() {
 		String sql = "SELECT BrukerNavn FROM ANSATT";
 		List<String> listOfUsernames = new ArrayList<String>();
 		connect();
 		try {
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			while(rs.next()){
+			while (rs.next()) {
 				listOfUsernames.add(rs.getString(1));
 			}
 		} catch (SQLException e) {
@@ -232,11 +262,12 @@ public class DatabaseController extends ApplicationComponent {
 		disconnect();
 		return listOfUsernames;
 	}
-	
-	public int[] deleteUser(String username){
-		String sql = "DELETE FROM ANSATT WHERE BrukerNavn='"+username+"'";
-		String sql2 = "DELETE FROM PAMINNELSE WHERE SendtTil='"+username+"'";
-		int rowseffected[] = {-1,-1};
+
+	public int[] deleteUser(String username) {
+		String sql = "DELETE FROM ANSATT WHERE BrukerNavn='" + username + "'";
+		String sql2 = "DELETE FROM PAMINNELSE WHERE SendtTil='" + username
+				+ "'";
+		int rowseffected[] = { -1, -1 };
 		connect();
 		try {
 			Statement st = con.createStatement();
@@ -250,11 +281,11 @@ public class DatabaseController extends ApplicationComponent {
 		disconnect();
 		return rowseffected;
 	}
-	
-	
-	public boolean editAppointment(int ID, String kolonne, String updatedTo){
+
+	public boolean editAppointment(int ID, String kolonne, String updatedTo) {
 		int rowsUpdated = -1;
-		String sql = "UPDATE AVTALE SET "+ kolonne + "=" + incapsulate(updatedTo) + " WHERE AvtaleID="+ID;
+		String sql = "UPDATE AVTALE SET " + kolonne + "="
+				+ incapsulate(updatedTo) + " WHERE AvtaleID=" + ID;
 		connect();
 		try {
 			Statement st = con.createStatement();
@@ -264,15 +295,15 @@ public class DatabaseController extends ApplicationComponent {
 			e.printStackTrace();
 		}
 		disconnect();
-		if(rowsUpdated >= 1)
+		if (rowsUpdated >= 1)
 			return true;
 		return false;
-		
+
 	}
-	
-	public boolean tryDeleteAppointment(int ID){
-		String sql = "UPDATE AVTALE SET ErAktiv=0 WHERE AvtaleID="+ID;
-		int res=-1;
+
+	public boolean tryDeleteAppointment(int ID) {
+		String sql = "UPDATE AVTALE SET ErAktiv=0 WHERE AvtaleID=" + ID;
+		int res = -1;
 		connect();
 		try {
 			Statement st = con.createStatement();
@@ -286,31 +317,34 @@ public class DatabaseController extends ApplicationComponent {
 			return true;
 		return false;
 	}
-	
-	public List<Room> retrieveAllRooms(){
+
+	public List<Room> retrieveAllRooms() {
 		String sql = "SELECT * FROM ROM";
 		List<Room> allRooms = new ArrayList<Room>();
 		try {
 			connect();
 			Statement st = con.createStatement();
 			ResultSet roomRowIterator = st.executeQuery(sql);
-			
-			while(roomRowIterator.next()){
-				Room a = new Room(roomRowIterator.getString(1), roomRowIterator.getInt(3), roomRowIterator.getString(2));
+
+			while (roomRowIterator.next()) {
+				Room a = new Room(roomRowIterator.getString(1),
+						roomRowIterator.getInt(3), roomRowIterator.getString(2));
 				allRooms.add(a);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		disconnect();
-		return allRooms;	
+		return allRooms;
 	}
-	
-	public boolean isRoomAvailable(String roomID, Date date, Time starttid, Time varighet){
+
+	public boolean isRoomAvailable(String roomID, Date date, Time starttid,
+			Time varighet) {
 		boolean isAvailable = true;
-		String sql = "SELECT dato, starttid, varighet FROM AVTALE WHERE avtalerom='"+roomID+"'";
-		date.setYear(date.getYear()-1900);
-		date.setMonth(date.getMonth()-1);
+		String sql = "SELECT dato, starttid, varighet FROM AVTALE WHERE avtalerom='"
+				+ roomID + "'";
+		date.setYear(date.getYear() - 1900);
+		date.setMonth(date.getMonth() - 1);
 		Date DBdate = null;
 		Time DBstart = null;
 		Time DBslutt = null;
@@ -320,43 +354,49 @@ public class DatabaseController extends ApplicationComponent {
 			connect();
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			
-			while(rs.next()){
+
+			while (rs.next()) {
 				DBdate = rs.getDate(1);
-				DBstart = new Time( rs.getTime(2).getHours(), rs.getTime(2).getMinutes());
-				DBslutt = new Time ( rs.getTime(2).getHours() + rs.getTime(3).getHours(), rs.getTime(2).getMinutes() + rs.getTime(3).getMinutes());
-				sluttid = new Time(starttid.returnHours()+varighet.returnHours(), starttid.returnMinutes()+varighet.returnMinutes());
+				DBstart = new Time(rs.getTime(2).getHours(), rs.getTime(2)
+						.getMinutes());
+				DBslutt = new Time(rs.getTime(2).getHours()
+						+ rs.getTime(3).getHours(), rs.getTime(2).getMinutes()
+						+ rs.getTime(3).getMinutes());
+				sluttid = new Time(starttid.returnHours()
+						+ varighet.returnHours(), starttid.returnMinutes()
+						+ varighet.returnMinutes());
 				eq = starttid.between(DBstart, DBslutt);
-				if(date.equals(DBdate)){
-					if( starttid.between(DBstart, DBslutt) || sluttid.between(DBstart, DBslutt)  ){
+				if (date.equals(DBdate)) {
+					if (starttid.between(DBstart, DBslutt)
+							|| sluttid.between(DBstart, DBslutt)) {
 						isAvailable = false;
-					}else if (DBstart.between(starttid, sluttid) || DBslutt.between(starttid, sluttid)){
-						isAvailable =false;
+					} else if (DBstart.between(starttid, sluttid)
+							|| DBslutt.between(starttid, sluttid)) {
+						isAvailable = false;
 					}
 				}
-				
+
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		disconnect();
-		
-		
-		return isAvailable ;
+
+		return isAvailable;
 	}
-	
+
 	public List<Meeting> retrieveMeetings(User user) {
 
 		String username = user.getUsername();
 		List<Meeting> listOfMeetings = new ArrayList<Meeting>();
-		String sql = "SELECT * FROM AVTALE WHERE AvtaleEier='"+username+"' " +
-				"AND ErAktiv=1 AND TYPE='Møte'";
+		String sql = "SELECT * FROM AVTALE WHERE AvtaleEier='" + username
+				+ "' " + "AND ErAktiv=1 AND TYPE='Møte'";
 		try {
 			connect();
 			Statement st = con.createStatement();
 			ResultSet rs = st.executeQuery(sql);
-			
-			while(rs.next()){
+
+			while (rs.next()) {
 				int ID = rs.getInt(1);
 				String roomName = rs.getString(6);
 				Date date = rs.getDate(3);
@@ -365,29 +405,31 @@ public class DatabaseController extends ApplicationComponent {
 				String title = rs.getString(2);
 				String desc = rs.getString(10);
 				String place = rs.getString(7);
-				
-				
-				String sql2 = "SELECT SendtTil FROM PAMINNELSE WHERE AvtaleID="+ID;
+
+				String sql2 = "SELECT SendtTil FROM PAMINNELSE WHERE AvtaleID="
+						+ ID;
 				Statement st2 = con.createStatement();
 				ResultSet rs2 = st2.executeQuery(sql2);
 				ArrayList<String> listOfParticipants = new ArrayList<String>();
-				while (rs2.next()){
+				while (rs2.next()) {
 					listOfParticipants.add(rs2.getString(1));
 				}
-				
+
 				Room room = null;
-				String sql3 = "SELECT * FROM ROM WHERE RomID='"+roomName+"'";
+				String sql3 = "SELECT * FROM ROM WHERE RomID='" + roomName
+						+ "'";
 				Statement st3 = con.createStatement();
 				ResultSet rs3 = st3.executeQuery(sql3);
-				while(rs3.next()){
-					room = new Room(rs3.getString(1), rs3.getInt(3), rs3.getString(2));
+				while (rs3.next()) {
+					room = new Room(rs3.getString(1), rs3.getInt(3),
+							rs3.getString(2));
 				}
-				//System.out.println(ID+"+");
+				// System.out.println(ID+"+");
 				Meeting m = new Meeting(ID, date, sTime, dur, title, desc,
 						place, listOfParticipants, room);
-				//System.out.println(m.getID()+"-");
+				// System.out.println(m.getID()+"-");
 				listOfMeetings.add(m);
-				
+
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -395,10 +437,10 @@ public class DatabaseController extends ApplicationComponent {
 		disconnect();
 		return listOfMeetings;
 	}
-	
-	public boolean summonToMeeting(String username, int appointmentID){
-		String sql = "INSERT INTO PAMINNELSE VALUES ('"+username+"', " +
-				""+appointmentID+", 'NULL', -1, 'Nytt')";
+
+	public boolean summonToMeeting(String username, int appointmentID) {
+		String sql = "INSERT INTO PAMINNELSE VALUES ('" + username + "', " + ""
+				+ appointmentID + ", 'NULL', -1, 'Nytt')";
 		int rowsAffected = -1;
 		connect();
 		try {
@@ -409,25 +451,57 @@ public class DatabaseController extends ApplicationComponent {
 			e.printStackTrace();
 		}
 		disconnect();
-		if (rowsAffected >=1)
+		if (rowsAffected >= 1)
 			return true;
 		return false;
 	}
-	
-	private String incapsulate(String input){
+
+
+	public List<Notification> retrieveNotifications(User user) {
+		String sql = "SELECT * FROM PAMINNELSE WHERE SendtTil = '"
+				+ user.getUsername() + "' AND SkalDelta=-1";
+		connect();
+		List<Notification> notifications = new ArrayList<Notification>();
+		Appointment appointment =  null;
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while (rs.next()) {
+				String sql2 = "SELECT * FROM AVTALE WHERE AvtaleID="
+						+ rs.getInt(2);
+				Statement st2 = con.createStatement();
+				ResultSet rs2 = st2.executeQuery(sql2);
+
+				while (rs2.next()) {
+					appointment = new Appointment(rs2.getInt(1),
+							rs2.getDate(3), rs2.getTime(8), rs2.getTime(9),
+							rs2.getString(2), rs2.getString(10),
+							rs2.getString(7));
+				}
+				String s = rs.getString(5);
+				int i = rs.getInt(4);
+				
+
+				 Notification n = new Notification(rs.getString(3),
+						string2enum.get(s), int2enum.get(i), appointment, user);
+				 notifications.add(n);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		disconnect();
+		return notifications;
+	}
+
+	private String incapsulate(String input) {
 		return "'" + input + "'";
 	}
-	
-	
-//	public static void main(String[] args) {
-//		DatabaseController dbc = new DatabaseController(null);
-//		User u = new User("dzedumpor", "mamma");
-//		List<Meeting> b = dbc.retrieveMeetings(u);
-//
-//		for (int i = 0; i < b.size(); i++) {
-//			System.out.println(b.get(i).getID());
-//		}
-//
-//	}
-}
 
+	public static void main(String[] args) {
+		DatabaseController dbc = new DatabaseController(null);
+		User u = new User("magrodahl", "mamma");
+		dbc.retrieveNotifications(u);
+
+	}
+}
