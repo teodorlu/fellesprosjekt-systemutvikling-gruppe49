026e7db31a2cce31a80ca5f8ce6438668e897d1b
Dzenan <Dzenan@Dzenan-PC.wlan.ntnu.no>
2012-03-28 13:53:37 +0200

    Summon har en trycatch for ? sjekke at ID er et tall

---
+++ src/model/Person.java
@@ -0,0 +1,62 @@
+package model;
+
+public class Person extends User {
+	
+	private String firstName, lastName, email;
+	
+	private boolean isOnline;
+	private UserCalendar personalCalendar;
+	
+	public Person(	String username,
+					String password,
+					String firstName,
+					String lastName,
+					String email) {
+		
+		super(username, password);
+		
+		String[] args = {firstName, lastName};
+		for (String s : args)
+			if (s.isEmpty())
+				throw new IllegalArgumentException();
+		
+		this.firstName = Character.toUpperCase(firstName.charAt(0)) + firstName.substring(1);
+		this.lastName = Character.toUpperCase(lastName.charAt(0)) + lastName.substring(1);
+		this.email = email;
+	}
+	
+	public Person(	String username,
+			String password,
+			String firstName,
+			String lastName,
+			String email,
+			boolean isOnline,
+			UserCalendar calendar) {
+		this(username, password, firstName, lastName, email);
+		
+		this.isOnline = isOnline;
+		this.personalCalendar = calendar;
+	}
+	
+	public String toString() {
+		String desc = super.toString() + ", name: " + lastName + ", " + firstName;
+		return desc;
+	}
+	
+	public String getFirstName(){
+		return firstName;
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public UserCalendar getPersonalCalendar() {
+		return personalCalendar;
+	}
+
+	public String getLastName(){
+		return lastName;
+	}
+}
